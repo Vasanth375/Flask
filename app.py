acdcbbd221dc67683bd5b9be40624ec63ec9ac0b
vasanth375 <vasanthnimmala69@outlook.com>
2022-03-19 22:59:46 +0530

    flask

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 # program on dynamic html templates
-
 # importing libraries
 from flask import Flask, render_template
 
 # creating flask object
 app=Flask(__name__)
 
-# home link 
-# @app.route('/')
-# def hello():
-#     return "hello this is main page <h1> hello </h1>"
-
 # this is the dynamic html example
-# whatever the 
+# whatever the hold variable you search on search bar that will display on the page
 @app.route('/<hold>')
 def hello(hold):
     return render_template('home.html', hold=hold)
+
 
 @app.route('/joker/<name>')
 def ok(name):
